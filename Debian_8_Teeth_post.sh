1c56a374f5d1894ff86e877b0b179e59514f7b21
davi4261 <david.kludt@rackspace.com>
2017-12-15 08:00:28 -0600

    Update and cleanup Debian 8 teeth templates

--- Debian_8_Teeth_post.sh
+++ Debian_8_Teeth_post.sh
@@ -5,9 +5,11 @@ parted -s /dev/sda set 1 boot on
 e2label /dev/sda1 root
 
 # replace the mdadm.conf w/ default universal config
-echo "CREATE owner=root group=disk mode=0660 auto=yes" > /etc/mdadm/mdadm.conf
-echo "HOMEHOST <system>" >> /etc/mdadm/mdadm.conf
-echo "MAILADDR root" >> /etc/mdadm/mdadm.conf
+cat > /etc/mdadm/mdadm.conf <<'EOF'
+CREATE owner=root group=disk mode=0660 auto=yes
+HOMEHOST <system>
+MAILADDR root
+EOF
 
 cat > /etc/initramfs-tools/conf.d/mdadm<<'EOF'
 ## mdadm boot_degraded configuration
@@ -16,13 +18,12 @@ BOOT_DEGRADED=true
 EOF
 
 # Fix mdadm config
-cp /usr/share/initramfs-tools/scripts/mdadm-functions /etc/initramfs-tools/scripts/
 cp /usr/share/initramfs-tools/hooks/mdadm /etc/initramfs-tools/hooks/
 cp /lib/udev/rules.d/64-md-raid-assembly.rules /etc/udev/rules.d/
 cp /lib/udev/rules.d/63-md-raid-arrays.rules /etc/udev/rules.d/
 
 wget http://KICK_HOST/misc/mdadm-init-deb -O /etc/initramfs-tools/scripts/local-top/mdadm
-chmod a+x /etc/initramfs-tools/scripts/local-top/mdadm 
+chmod a+x /etc/initramfs-tools/scripts/local-top/mdadm
 
 # Persist our custom udev rules for raid support
 cat > /etc/udev/rules.d/21-persistent-local.rules<<'EOF'
@@ -33,11 +34,8 @@ EOF
 # Set udev rule to not add by-label symlinks for v2 blockdevs if not raid
 wget http://KICK_HOST/misc/60-persistent-storage.rules-deb7 -O /etc/udev/rules.d/60-persistent-storage.rules
 cat /etc/udev/rules.d/21-persistent-local.rules >> /etc/udev/rules.d/60-persistent-storage.rules
-update-initramfs -u
 
-# teeth cloud-init workaround, hopefully goes away with upstream cloud-init changes?
-#wget http://KICK_HOST/kickstarts/Teeth-cloud-init
-#cp Teeth-cloud-init /usr/share/pyshared/cloudinit/sources/DataSourceConfigDrive.py
+# teeth cloud-init workaround
 wget http://KICK_HOST/cloud-init/cloud-init_0.7.7-py3.4-systemd.deb
 dpkg -i *.deb
 apt-mark hold cloud-init
@@ -98,10 +96,6 @@ ff02::1 ip6-allnodes
 ff02::2 ip6-allrouters
 EOF
 
-# set some stuff
-#echo 'net.ipv4.conf.eth0.arp_notify = 1' >> /etc/sysctl.conf
-#echo 'vm.swappiness = 0' >> /etc/sysctl.conf
-
 cat >> /etc/sysctl.conf <<'EOF'
 net.ipv4.tcp_rmem = 4096 87380 33554432
 net.ipv4.tcp_wmem = 4096 65536 33554432
@@ -121,23 +115,58 @@ sed -i '/.*cdrom.*/d' /etc/apt/sources.list
 
 # keep grub2 from using UUIDs and regenerate config
 sed -i 's/#GRUB_DISABLE_LINUX_UUID.*/GRUB_DISABLE_LINUX_UUID="true"/g' /etc/default/grub
-#sed -i 's/#GRUB_TERMINAL=console/GRUB_TERMINAL=console/g' /etc/default/grub
 sed -i 's/#GRUB_TERMINAL=console/GRUB_TERMINAL=/g' /etc/default/grub
-#sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT.*/GRUB_CMDLINE_LINUX_DEFAULT="console=ttyS4,115200n8 8250.nr_uarts=5 splash quiet"/g' /etc/default/grub
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT.*/GRUB_CMDLINE_LINUX_DEFAULT="rd.fstab=no acpi=noirq noapic 8250.nr_uarts=5 quiet"/g' /etc/default/grub
+
+# Adding numa=off due to this bug: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=883938
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT.*/GRUB_CMDLINE_LINUX_DEFAULT="rd.fstab=no acpi=noirq numa=off noapic 8250.nr_uarts=5"/g' /etc/default/grub
 sed -i 's/GRUB_TIMEOUT.*/GRUB_TIMEOUT=0/g' /etc/default/grub
-#echo 'GRUB_SERIAL_COMMAND="serial --unit=0 --speed=115200n8 --word=8 --parity=no --stop=1"' >> /etc/default/grub
 echo 'GRUB_DEVICE_LABEL=root' >> /etc/default/grub
 update-grub
 
 # TODO: make update-grub persist label boot
 sed -i 's#/dev/sda1#LABEL=root#g' /boot/grub/grub.cfg
 
-#add this to make sure it gets copied to initrd
-echo "INITRDSTART='all'" >> /etc/default/mdadm
-
-echo "sleep 9" > /etc/initramfs-tools/scripts/init-premount/delay_for_raid
+cat >> /etc/initramfs-tools/scripts/init-premount/delay_for_raid <<'EOF'
+#!/bin/sh
+/bin/sleep 9
+EOF
 chmod a+x /etc/initramfs-tools/scripts/init-premount/delay_for_raid
+
+# Add assemble script to check for RAID completion. Due to bug: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=814036
+cat >> /etc/initramfs-tools/scripts/local-top/assemble_raid <<'EOF'
+#!/bin/sh
+PREREQ="mdadm"
+prereqs()
+{
+   echo "$PREREQ"
+}
+
+case $1 in
+prereqs)
+   prereqs
+   exit 0
+   ;;
+esac
+
+. /scripts/functions
+
+log_begin_msg "Attempting to assemble RAID array"
+
+if [ -e /dev/md127 ]; then
+    log_warning_msg "Found /dev/md127"
+    if [ -e /dev/md126 ]; then
+        log_warning_msg "RAID 1 assembled"
+    else
+        log_warning_msg "Assembling RAID array"
+        /sbin/mdadm -Iv /dev/md127
+    fi
+else
+    log_warning_msg "/dev/md127 does not exist so nothing to assemble"
+fi
+log_end_msg
+exit 0
+EOF
+chmod a+x /etc/initramfs-tools/scripts/local-top/assemble_raid
 
 # fix growpart for raid
 wget http://KICK_HOST/misc/growroot -O /usr/share/initramfs-tools/scripts/local-bottom/growroot
@@ -148,9 +177,11 @@ chmod a+x /usr/bin/growpart
 # another teeth specific
 echo "bonding" >> /etc/modules
 echo "8021q" >> /etc/modules
+
 cat > /etc/modprobe.d/blacklist-mei.conf <<'EOF'
 blacklist mei_me
 EOF
+
 update-initramfs -u -k all
 
 # more teeth console changes
@@ -183,7 +214,6 @@ rm -f /etc/ssh/ssh_host_*
 rm -f /var/cache/apt/archives/*.deb
 rm -f /var/cache/apt/*cache.bin
 rm -f /var/lib/apt/lists/*_Packages
-#rm -f /etc/resolv.conf
 rm -f /root/.bash_history
 rm -f /root/.nano_history
 rm -f /root/.lesshst
